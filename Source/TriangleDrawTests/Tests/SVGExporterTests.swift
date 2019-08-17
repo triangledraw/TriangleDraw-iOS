a50edcd9ef800ec200d21a1a7fbb1130c015a3d7
Simon Strandgaard <neoneye@gmail.com>
2019-08-17 13:04:30 +0200

    Cleaned up test of SVGExporter

--- Source/TriangleDrawTests/Tests/SVGExporterTests.swift
+++ Source/TriangleDrawTests/Tests/SVGExporterTests.swift
@@ -4,21 +4,25 @@ import XCTest
 @testable import TriangleDrawMain
 
 class SVGExporterTests: XCTestCase {
-
-    func testExample() {
-//		let canvas: E2Canvas = loadCanvas("test_subdivide2_in.pbm")
-//		let canvas: E2Canvas = loadCanvas("test_rotate_logo_none.txt")
-//		let canvas: E2Canvas = loadCanvas("test_boundingbox0.txt")
-//		let canvas: E2Canvas = loadCanvas("test_boundingbox3.txt")
-//		let canvas: E2Canvas = loadCanvas("test_exportsvg_corners.pbm")
-		let canvas: E2Canvas = loadCanvas("test_exportsvg_cube.pbm")
-
+    func test0() {
+		let canvas: E2Canvas = loadCanvas("test_exportsvg_corners.pbm")
 		let exporter = SVGExporter(canvas: canvas)
-		exporter.appVersion = "2019.2.1"
-		exporter.rotated = true
+		exporter.rotated = false
 		let data = exporter.generateData()
-		let url: URL = URL(fileURLWithPath: "/Users/neoneye/Desktop/result.svg").absoluteURL
-		try! data.write(to: url)
+		XCTAssertGreaterThan(data.count, 50000)
+		XCTAssertLessThan(data.count, 500000)
+//		let url: URL = URL(fileURLWithPath: "/Users/neoneye/Desktop/result0.svg").absoluteURL
+//		try! data.write(to: url)
     }
 
+	func test1() {
+		let canvas: E2Canvas = loadCanvas("test_exportsvg_cube.pbm")
+		let exporter = SVGExporter(canvas: canvas)
+		exporter.rotated = true
+		let data = exporter.generateData()
+		XCTAssertGreaterThan(data.count, 50000)
+		XCTAssertLessThan(data.count, 500000)
+//		let url: URL = URL(fileURLWithPath: "/Users/neoneye/Desktop/result1.svg").absoluteURL
+//		try! data.write(to: url)
+	}
 }
