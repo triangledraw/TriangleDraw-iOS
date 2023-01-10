123229c3c6d224177918bc24827d074b040f459d
Simon Strandgaard <neoneye@gmail.com>
2023-01-10 11:52:26 +0100

    Updated copyright year

--- Source/TriangleDrawLibrary/Engine/E2CanvasBoundingBox.swift
+++ Source/TriangleDrawLibrary/Engine/E2CanvasBoundingBox.swift
@@ -1,4 +1,4 @@
-// MIT license. Copyright (c) 2021 TriangleDraw. All rights reserved.
+// MIT license. Copyright (c) 2023 TriangleDraw. All rights reserved.
 
 public struct E2CanvasBoundingBox {
 	/// Index of the left-most column where the user have drawn content.
